d45f9e4afc03f3774d69bac65d0bf587bde755e7
Shawdooow <banhammer1915@gmail.com>
2018-04-05 02:02:17 -0400

    fix

---
+++ Vitaru.Plot/Story/Background.cs
@@ -0,0 +1,95 @@
+﻿namespace Vitaru.Plot.Story
+{
+    public class Background
+    {
+        public const string ReimuWiki1 = "\n\nReimu used to be a complete air head. " +
+            "But time and hardship has shaped her into the strong cunning magician she is today. " +
+            "Usually you would be hard-pressed to not only get the jump on her but even find her before she finds you. " +
+            "However don't let this fool you, she is by no means a rutheless killer like some of her friends, infact she can be quite sweet. " +
+            "Just try not to get on her bad side. ";
+
+        /// <summary>
+        /// Hint to start Touhosu mode from the side of Ignorance
+        /// </summary>
+        public const string ReimuWiki2 = "Though lately she seems to be spacing out again, almost like she is off in her own world. . .\n\n" +
+            "And indeed she is, having nightmares of a night long gone.";
+
+        public const string MarisaWiki1 = "\n\nMarisa Kirisame, the magical witch of the forest who could do no wrong, or so they said. " +
+            "One thing that is certain is her lust for control, she never lets the situation get out of hand. " +
+            "Last time she did it cost her greatly, and created wounds that won't heal as easily as she would lead you to believe.";
+
+        //Quick overview of the shattered Sakuya we have at the begining
+        public const string SakuyaWiki1 = "\n\nYoung Sakuya used to be kind, caring, naive and most of all stubborn, like the ones who raised her. " +
+            "But even the purest of hearts can be broken given the right circumstances, corrupted by the dark things that lurk in the night. " +
+            "Physical wounds may heal, but the emotional stabbing she was subjected to can never be mended.";
+
+        /// <summary>
+        /// feel like this one is obvious, see Map.Map2
+        /// </summary>
+        public const string SakuyaWiki2 = "\n\nBut that happened a very long time ago and today is a new day in a new age. An age of reason.";
+
+        public const string SakuyaWiki3 = "\n\nYoung Sakuya used to be kind, caring, naive and most of all stubborn, like the ones who raised her. " +
+            "But even the purest of hearts can be broken given the right circumstances, corrupted by the dark things that lurk in the night. " +
+            "Physical wounds may heal, but the emotional stabbing she was subjected to took much longer to mend.";
+
+        //A past story of how Hong met Sakuya
+        public const string HongWiki1 = "\n\nHong was your typical war hero. She fought valiantly, saved allies, showed no mercy against the enemy. " +
+            "She didn't really care for all the medals or attention though, now that the war was over she just wanted to retire home.\n\n" +
+            "Upon returning home she met with the Scarlet sisters she had entrusted the house with, ony to find they are different. " +
+            "They had wings, they grew wings? They were fairies now? " +
+            "No, Remilia was a vampire now, Flan is a Fairy." +
+            "Hong didn't mind as they were still dear friends and dispite having a track record of mishieve, the fact that the mansion was still standing was a testement to their capabilities.\n\n" +
+            "Thats not all though, they also had a small child with them, she couldn't have been older than five or six years old. " +
+            "She had grey eyes and grey hair, they dressed her in Hong's old maid uniform, the blue one, and begun to teach her how to keep the place in order. " +
+            "Hong hadn't said a word yet, and didn't. " +
+            "She went straight to her room and propmtly fell asleep on her bed, which felt even softer than before she left.\n\n" +
+            "She awoke to the small child holding a tea set at her bed side. " +
+            "She stood straight as a pencil, proper maid edicate.\n\n" +
+            "Hong reached for one of the tea cups that had already been poured next to a note that read: \"Hope you still like 'Green Moon'. -Remilia\"\n" +
+            "She took a sip then began: \"Thank you, how long have you been here?\"\n\n" +
+            "The little girl who sounded almost too fragile hesitated, she didn't expect anything to be spoken today: \"A few months? Its hard to keep track of time inside all day.\"\n\n" +
+            "\"Huh, they never let you go outside?\" Hong said taking the tray from the little girl.\n\n" +
+            "\"No, they said those jobs are too hard for me because I am still little, but sometimes I finish early and watch them through the windows. Cleaning the fountain doesn't look that hard.\"\n\n" +
+            "\"Come with me.\" she said finaly standing up and walking out into the hallway. " +
+            "She first went to the kitchen to drop off the tray then took the young girl out back. " +
+            "The Scarlet sisters where halfway through redoing the roofing and were up of the roof working on it. " +
+            "They exchanged waves with Hong and the girl then went back to working, it appeared as they had no struggle with it as they could fly now.\n\n" +
+            "\"I don't believe I have asked your name yet, do you mind sharing?\"\n\n" +
+            "\"The two sisters call me Sakeuta, apparently it means 'time waster' in some old language?\"\n\n" +
+            "\"Why would they say that?\"\n\n" +
+            "\"Sometimes I slack on cleaning dishes, it is just soo boring.\"\n\n" +
+            "\"It certainly is, but that will never do for a name.\" " +
+            "Hong was thinking, then she saw her staring at the shedding cherry blossoms over by the maze.\n\n" +
+            "\"How about Sakuya?\"\n\n" +
+            "\"Huh?\"\n\n" +
+            "\"Your name? How about Sakuya. It means 'time keeper' in an old arcane language I studied in my free time.\"\n\n" +
+            "\"Hmm, its better than 'time waster', I like it.\"\n\n" +
+            "Hong was wondering where Sakuya came from, and would question the Scarlet sisters later about. " +
+            "For now though, she was transfixed on how similar she is to Hong was when she was little, slightly defient but eager to help.\n\n" +
+            "\"Let me show you a few tricks to the jobs back here I bet the Scarlet sisters don't know.\"";
+
+        public const string FlandreWiki1 = "\n\nFlandre used to be one of the most feared fairies around, and thats no small feat. " +
+            "Fairies are have a tendency to be stupid, but Flandre wasn't always a Fairy now was she?\n\n" +
+            "Now days all she does is mindless games in the basement, broken but not lost. " +
+            "One day she could return, and you wouldn't want to be on the recieving end of her wrath.";
+
+        public const string RemiliaWiki1 = "\n\nRemilia wasn't always a vampire, she didn't always have a thirst for blood. " +
+            "But things change, something happened one day and she was 'ascended' she keeps telling herself with her sister. " +
+            "Certainly this was a change for the better though, after all biological imortality is hard to come by.\n\n" +
+            "She also loves her sister Flandre dearly, but for a long time now she has been broken. " +
+            "Flan always used to be a bit loopy but now she won't even speak to Remilia anymore." +
+            "All she does is dwell in the basement all day playing make believe games alone in the dark. " +
+            "Perhaps one day she may be put back together, however unlikely.\n\n" +
+            "For now this family shall remain shattered by un-imaginable pain, but the worst has yet to come.";
+
+        public const string RemiliaWiki2 = "";
+
+        public const string YukariWiki1 = "\n\nThere are many stories about Yukari, some say she was born of some rich folk to the far west and some say she predates the known universe itself. " +
+            "While that would explain her unatural abilities in combat they would not explain her uncanny abitlity to empithize with her \"creations\". " +
+            "The only other individual to supposedly be even relativly this old is cold and heartless (perhaps litterally).";
+
+        public const string VasterWiki1 = "\n\n Vaster never really understood the whole \"magic\" thing, he liked to keep things simple. " +
+            "Whats wrong with cutting through all your problems with a sword or just using a gun? " +
+            "He knew exactly why, you have to be on a level playing field to stand a chance.";
+    }
+}
---
+++ Vitaru.Plot/Story/CharacterSpeech/Sakuya.cs
@@ -0,0 +1,22 @@
+﻿namespace Vitaru.Plot.Story.CharacterSpeech
+{
+    public class Sakuya
+    {
+        public const string Late1 = "Can't be late. . .";
+        public const string Late2 = "Never late. . .";
+        public const string Late3 = "They aren't here?";
+        public const string Late4 = "No, I can still make it!";
+        public const string Late5 = "I'll just-";
+        public const string Late6 = "Huh?";
+        public const string Late7 = "I can't contol time anymore!";
+        public const string Late8 = "What is this!?";
+        public const string Late9 = "Remilia. . .";
+        public const string Late10 = "She must've put something in my tea this morning!";
+        public const string Late11 = "I swear, ";
+        public const string Late12 = "I am going kill her one of these days";
+        public const string Late13 = "Finally, I made it";
+        public const string Late14 = "Remilia, wheres you're guest?";
+        public const string Late15 = "What do you mean?\nAnd what did you put in my tea!?";
+        public const string Late16 = "What do you mean \"Its good to have me back\"?";
+    }
+}
