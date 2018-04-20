c8b01d10b41f27321712eab463e08f08e88f5d1d
Shawdooow <banhammer1915@gmail.com>
2018-04-19 23:30:38 -0400

    symcol

---
+++ osu.Game/Screens/Symcol/CasterBible/Pieces/MapBox.cs
@@ -0,0 +1,9 @@
+﻿using osu.Framework.Graphics.Containers;
+
+namespace osu.Game.Screens.Symcol.CasterBible.Pieces
+{
+    public class MapBox : Container
+    {
+
+    }
+}
---
+++ osu.Game/Screens/Symcol/CasterBible/Pieces/UTCClock.cs
@@ -0,0 +1,106 @@
+﻿using OpenTK;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Game.Graphics.Sprites;
+using System;
+
+namespace osu.Game.Screens.Symcol.CasterBible.Pieces
+{
+    public class UTCClock : Container
+    {
+        public int TextSize
+        {
+            get { return textSize;  }
+            set
+            {
+                if (value != textSize)
+                {
+                    textSize = value;
+
+                    hour.TextSize = value;
+                    minutes.TextSize = value;
+                    seconds.TextSize = value;
+                }
+            }
+        }
+
+        private int textSize = 20;
+
+        private OsuSpriteText hour;
+        private OsuSpriteText minutes;
+        private OsuSpriteText seconds;
+
+        public UTCClock()
+        {
+            Origin = Anchor.Centre;
+
+            Children = new Drawable[]
+            {
+                hour = new OsuSpriteText
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Position = new Vector2(-28, 0),
+
+                    TextSize = textSize,
+                    Text = "66"
+                },
+                new OsuSpriteText
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Position = new Vector2(-14, 0),
+
+                    TextSize = textSize,
+                    Text = ":"
+                },
+                minutes = new OsuSpriteText
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+
+                    TextSize = textSize,
+                    Text = "66"
+                },
+                new OsuSpriteText
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Position = new Vector2(14, 0),
+
+                    TextSize = textSize,
+                    Text = ":"
+                },
+                seconds = new OsuSpriteText
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Position = new Vector2(28, 0),
+
+                    TextSize = textSize,
+                    Text = "66"
+                },
+                new OsuSpriteText
+                {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Position = new Vector2(28 * 2, 0),
+
+                    TextSize = textSize,
+                    Text = "UTC"
+                },
+            };
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+
+            DateTime time = DateTime.UtcNow;
+
+            hour.Text = time.Hour.ToString();
+            minutes.Text = time.Minute.ToString();
+            seconds.Text = time.Second.ToString();
+        }
+    }
+}
