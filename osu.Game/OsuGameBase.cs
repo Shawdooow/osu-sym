932f33f1d569784d345771aeaad06691f56dd10f
Shawdooow <banhammer1915@gmail.com>
2018-05-14 10:24:00 -0400

    version number

--- osu.Game/OsuGameBase.cs
+++ osu.Game/OsuGameBase.cs
@@ -81,7 +81,7 @@ namespace osu.Game
                     //return @"local " + (DebugUtils.IsDebug ? @"debug" : @"release");
 
                 var assembly = AssemblyName;
-                return $@"2018.511.0";
+                return $@"2018.514.0";
             }
         }
 
