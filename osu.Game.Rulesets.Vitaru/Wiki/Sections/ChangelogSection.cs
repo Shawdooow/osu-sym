30275d79f0815f6cc0dc047c817a117fecb89934
Shawdooow <banhammer1915@gmail.com>
2018-05-23 14:22:36 -0400

    abstract changelog to rulesets core

--- osu.Game.Rulesets.Vitaru/Wiki/Sections/ChangelogSection.cs
+++ osu.Game.Rulesets.Vitaru/Wiki/Sections/ChangelogSection.cs
@@ -1,58 +1,18 @@
-﻿using osu.Framework.Allocation;
-using osu.Framework.Logging;
-using osu.Framework.Platform;
-using Symcol.Rulesets.Core.Wiki;
-using System.IO;
+﻿using Symcol.Rulesets.Core.Wiki;
 
 namespace osu.Game.Rulesets.Vitaru.Wiki.Sections
 {
-    //TODO: abstract this for all rulesets to use, its super useful!
-    public class ChangelogSection : WikiSection
+    public class ChangelogSection : WikiChangelogSection
     {
         public override string Title => "Changelog";
 
-        private const string file_extention = ".vitaru";
+        protected override string RulesetVersion => VitaruRuleset.RulesetVersion;
 
-        private const string version_changelog = "-Tweak debug ui\n" +
+        protected override string RulesetStorage => "vitaru";
+
+        protected override string FileExtention => ".vitaru";
+
+        protected override string VersionChangelog => "-Tweak debug ui\n" +
             "-Fix Combo fire never reseting back to 0 on miss";
-
-        [BackgroundDependencyLoader]
-        private void load(Storage storage)
-        {
-            Storage changelogStorage = storage.GetStorageForDirectory("vitaru");
-
-            Content.Add(new WikiParagraph("This changelog is cumulative, meaning only versions you install and run will be added here. " +
-                "They are saved as text files in your osu! storage. " +
-                "Below are all the versions you have saved + this version (" + VitaruRuleset.RulesetVersion + ")."));
-
-            try
-            {
-                if (changelogStorage == null)
-                    Logger.Log("changelogStorage == null", LoggingTarget.Database, LogLevel.Error);
-                else
-                {
-                    using (Stream stream = changelogStorage.GetStream(VitaruRuleset.RulesetVersion + file_extention, FileAccess.Write, FileMode.Create))
-                    using (StreamWriter w = new StreamWriter(stream))
-                        w.WriteLine(version_changelog);
-                }
-            }
-            catch { Logger.Log("Could not create / update current version file in changelogStorage!", LoggingTarget.Database, LogLevel.Error); }
-
-            try
-            {
-                for (int i = 10; i >= 0; i--)
-                    for (int j = 20; j >= 0; j--)
-                        for (int k = 20; k >= 0; k--)
-                            if (changelogStorage.Exists(i + "." + j + "." + k + file_extention))
-                            {
-                                Content.Add(new WikiSubSectionHeader(i + "." + j + "." + k));
-
-                                using (Stream stream = changelogStorage.GetStream(i + "." + j + "." + k + file_extention, FileAccess.Read, FileMode.Open))
-                                using (StreamReader r = new StreamReader(stream))
-                                    Content.Add(new WikiParagraph(r.ReadToEnd()));
-                            }
-            }
-            catch { Logger.Log("Error reading saved versions (if you have any, if not thats the problem likely)", LoggingTarget.Database, LogLevel.Error); }
-        }
     }
 }
