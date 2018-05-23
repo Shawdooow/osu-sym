db8c663ed97568ec329407cc576a218f23544e34
Shawdooow <banhammer1915@gmail.com>
2018-05-23 14:23:36 -0400

    use the classic bg in wiki header

--- osu.Game.Rulesets.Classic/Wiki/Sections/ClassicWikiHeader.cs
+++ osu.Game.Rulesets.Classic/Wiki/Sections/ClassicWikiHeader.cs
@@ -8,6 +8,8 @@ namespace osu.Game.Rulesets.Classic.Wiki
     {
         protected override Texture RulesetIcon => ClassicRuleset.ClassicTextures.Get("icon@2x");
 
+        protected override Texture HeaderBackground => ClassicRuleset.ClassicTextures.Get("osu!classic Thumbnail");
+
         protected override string RulesetName => "classic";
 
         protected override string RulesetDescription => "classic! is a 3rd party ruleset developed for osu!lazer. It seeks to be an identical copy of osu!stable's osu!standard for everyone who doesn't like the way lazer plays.";
