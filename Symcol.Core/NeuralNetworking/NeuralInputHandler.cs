7c45730036030eea971514bcb3deea7616d99be5
Shawdooow <banhammer1915@gmail.com>
2018-05-28 22:56:47 -0400

    updated neural networking structure

---
+++ Symcol.Core/NeuralNetworking/NeuralInputHandler.cs
@@ -0,0 +1,15 @@
+﻿using osu.Framework.Input.Bindings;
+
+namespace Symcol.Core.NeuralNetworking
+{
+    public abstract class NeuralInputHandler<T> : KeyBindingContainer<T>
+        where T : struct
+    {
+        public TensorFlowBrain TensorFlowBrain { get; private set; }
+
+        public NeuralInputHandler()
+        {
+            TensorFlowBrain = new TensorFlowBrain();
+        }
+    }
+}
