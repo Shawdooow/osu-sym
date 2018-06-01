82aff5f2e82b8cffcbbefef97a8b519430bad4f6
Shawdooow <banhammer1915@gmail.com>
2018-06-01 08:38:28 -0400

    change version number

--- osu.Game.Rulesets.Vitaru/Wiki/Sections/ChangelogSection.cs
+++ osu.Game.Rulesets.Vitaru/Wiki/Sections/ChangelogSection.cs
@@ -12,18 +12,14 @@ namespace osu.Game.Rulesets.Vitaru.Wiki.Sections
 
         protected override string FileExtention => ".vitaru";
 
-        protected override string VersionChangelog => "-Updated to lazer version 2018.531.0\n" +
-            "-Updated to lazer version 2018.526.0\n\n" +
+        protected override string VersionChangelog => "" +
             "Features:\n\n" +
-            "-Implement Reimu's Spell \"Leader\"\n\n" +
+            "\n\n" +
             "Tweaks / Changes:\n\n" +
-            "-Tweak Reimu's stats based on now implemented spell\n" +
-            "-Tweak debug ui\n" +
-            "-Give all patterns Non-Linear SpeedEasings (nerf spinners by making everything else harder)\n\n" +
+            "\n\n" +
             "Fixes:\n\n" +
-            "-Fix sliders not playing proper hitsounds (sliders not throwing proper patterns for hitsounds will get fixed in next release)" +
-            "-Fix Combo fire never reseting back to 0 on miss\n\n" + 
+            "\n\n" + 
             "Additional Patch-Notes:\n\n" +
-            "-Started work on Auto under the hood (both hardcoded ai and neural networking)";
+            "";
     }
 }
