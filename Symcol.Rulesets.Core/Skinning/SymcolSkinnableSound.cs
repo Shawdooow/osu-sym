3c6077296d18e0a3b9c1f95e9800153b8865765f
Shawdooow <banhammer1915@gmail.com>
2018-05-31 21:52:52 -0400

    fucking drawable sounds are good code Dean

---
+++ Symcol.Rulesets.Core/Skinning/SymcolSkinnableSound.cs
@@ -0,0 +1,25 @@
+﻿using osu.Framework.Graphics.Containers;
+using osu.Game.Audio;
+using osu.Game.Skinning;
+
+namespace Symcol.Rulesets.Core.Skinning
+{
+    public class SymcolSkinnableSound : SkinnableSound
+    {
+        public SymcolSkinnableSound(params SampleInfo[] samples) : base(samples)
+        {
+        }
+
+        private bool deleted;
+
+        public virtual void Delete()
+        {
+            if (Parent is Container p)
+                p.Remove(this);
+
+            deleted = true;
+
+            Dispose();
+        }
+    }
+}
