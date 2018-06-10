9a8e298094c7a48c7ec8d41ea4d80929c364ff20
Shawdooow <banhammer1915@gmail.com>
2018-06-10 13:58:48 -0400

    make it even better

--- osu.Game.Rulesets.Vitaru/UI/VitaruRulesetContainer.cs
+++ osu.Game.Rulesets.Vitaru/UI/VitaruRulesetContainer.cs
@@ -32,7 +32,7 @@ namespace osu.Game.Rulesets.Vitaru.UI
             if (DebugToolkit.MachineLearningDebugItems.Count > 0)
                 DebugToolkit.MachineLearningDebugItems = new List<Container>();
 
-            DebugToolkit.GeneralDebugItems.Add(ranked = new DebugStat<int>(new Bindable<int>()) { Text = VitaruPlayfield.OnJudgement != null ? "Ranked" : "Unranked (Bad Code)" });
+            DebugToolkit.GeneralDebugItems.Add(ranked = new DebugStat<int>(new Bindable<int>()));
             VitaruPlayfield = new VitaruPlayfield((VitaruInputManager)KeyBindingInputManager);
         }
 
@@ -40,6 +40,7 @@ namespace osu.Game.Rulesets.Vitaru.UI
         {
             base.LoadComplete();
 
+            ranked.Text = VitaruPlayfield.OnJudgement != null ? "Ranked" : "Unranked (Bad Code)";
             VitaruInputManager vitaruInputManager = (VitaruInputManager)KeyBindingInputManager;
             vitaruInputManager.DebugToolkit?.UpdateItems();
         }
