b7a984c15c9e47dbc491242b3b725ea393237434
Shawdooow <banhammer1915@gmail.com>
2018-07-09 15:42:30 -0400

    tweak drain values

--- osu.Game.Rulesets.Vitaru/Characters/TouhosuPlayers/Hakurei/Drawables/DrawableRyukoy.cs
+++ osu.Game.Rulesets.Vitaru/Characters/TouhosuPlayers/Hakurei/Drawables/DrawableRyukoy.cs
@@ -93,7 +93,7 @@ namespace osu.Game.Rulesets.Vitaru.Characters.TouhosuPlayers.Hakurei.Drawables
                             break;
                     }
 
-                Energy -= Clock.ElapsedFrameTime / 1000 * (bullets * 0.2f + players * 0.5f + enemies * 0.25f + bosses * 2f);
+                Energy -= Clock.ElapsedFrameTime / 1000 * (bullets * 0.1f + players * 0.25f + enemies * 0.2f + bosses * 1f);
             }
             else if (Untuned)
                 Untuned = false;
