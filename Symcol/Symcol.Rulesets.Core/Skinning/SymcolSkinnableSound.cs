17b87ad339a80347ab6fb0da68549faf41c47c25
Shawdooow <banhammer1915@gmail.com>
2018-07-12 10:25:43 -0400

    RulesetAudio

--- Symcol/Symcol.Rulesets.Core/Skinning/SymcolSkinnableSound.cs
+++ Symcol/Symcol.Rulesets.Core/Skinning/SymcolSkinnableSound.cs
@@ -1,4 +1,9 @@
-﻿using osu.Framework.Audio;
+﻿using System;
+using System.Linq;
+using osu.Framework.Allocation;
+using osu.Framework.Audio;
+using osu.Framework.Audio.Sample;
+using osu.Framework.Extensions.IEnumerableExtensions;
 using osu.Framework.Graphics.Containers;
 using osu.Game.Audio;
 using osu.Game.Skinning;
@@ -7,14 +12,56 @@ namespace Symcol.Rulesets.Core.Skinning
 {
     public class SymcolSkinnableSound : SkinnableSound
     {
-        public AudioManager RulesetAudio;
-
         public override bool HandleMouseInput => false;
         public override bool HandleKeyboardInput => false;
 
-        public SymcolSkinnableSound(params SampleInfo[] samples) : base(samples)
+        public AudioManager RulesetAudio;
+
+        private readonly SampleInfo[] samples;
+        private SampleChannel[] channels;
+
+        private AudioManager audio;
+
+        public SymcolSkinnableSound(params SampleInfo[] samples) : base(new SampleInfo[]{})
         {
             Name = "SymcolSkinnableSound";
+            this.samples = samples;
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(AudioManager audio)
+        {
+            this.audio = audio;
+        }
+
+        public void Play() => channels?.ForEach(c => c.Play());
+
+        protected override void SkinChanged(ISkinSource skin, bool allowFallback)
+        {
+            channels = samples.Select(s =>
+            {
+                var ch = loadChannel(s, skin.GetSample);
+                if (ch == null && allowFallback)
+                    ch = loadChannel(s, RulesetAudio.Sample.Get);
+                if (ch == null && allowFallback)
+                    ch = loadChannel(s, audio.Sample.Get);
+                return ch;
+            }).Where(c => c != null).ToArray();
+        }
+
+        private SampleChannel loadChannel(SampleInfo info, Func<string, SampleChannel> getSampleFunction)
+        {
+            foreach (var lookup in info.LookupNames)
+            {
+                var ch = getSampleFunction($"Gameplay/{lookup}");
+                if (ch == null)
+                    continue;
+
+                ch.Volume.Value = info.Volume / 100.0;
+                return ch;
+            }
+
+            return null;
         }
 
         protected override void LoadComplete()
