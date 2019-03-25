9af29e867f17f3c05cea9c254ebc4b05107933db
Shawdooow <banhammer1915@gmail.com>
2019-03-24 21:20:31 -0400

    visual fixes

--- osu.Game.Rulesets.Vitaru/Mods/Sym/Wiki/Sections/Changelog/_0_4.cs
+++ osu.Game.Rulesets.Vitaru/Mods/Sym/Wiki/Sections/Changelog/_0_4.cs
@@ -2,8 +2,6 @@
 {
     internal class _0_4_0 : ChangelogVersion
     {
-        public override string VersionTitle => "";
-
         public override string VersionNumber => "0.4.0";
 
         public override string GetChangelog()
--- osu.Game.Rulesets.Vitaru/Mods/Sym/Wiki/Sections/Changelog/_0_5.cs
+++ osu.Game.Rulesets.Vitaru/Mods/Sym/Wiki/Sections/Changelog/_0_5.cs
@@ -2,8 +2,6 @@
 {
     internal class _0_5_0 : ChangelogVersion
     {
-        public override string VersionTitle => "";
-
         public override string VersionNumber => "0.5.0";
 
         public override string GetChangelog()
