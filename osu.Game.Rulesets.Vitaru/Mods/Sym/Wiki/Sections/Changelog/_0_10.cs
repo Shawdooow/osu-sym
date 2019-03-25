2f4e426913dac9a45f1ffc2cbe9f6d2475472497
Shawdooow <banhammer1915@gmail.com>
2019-03-24 21:17:57 -0400

    tweaks

--- osu.Game.Rulesets.Vitaru/Mods/Sym/Wiki/Sections/Changelog/_0_10.cs
+++ osu.Game.Rulesets.Vitaru/Mods/Sym/Wiki/Sections/Changelog/_0_10.cs
@@ -18,19 +18,23 @@
             "[WIP] Patterns V3",
             "Scoring V3",
             "[Experimental] New bullet visuals",
-            "[Experimental] Touhou sounds",
-            "[Desktop] Score Submission (Leaderboards don't work but scores are uploaded and saved when connected)",
+            //"Added classic enemy visuals",
+            "[Experimental] Touhou sounds\n",
+
+            "Score Submission (Leaderboards don't work but scores are uploaded and saved when connected)",
             "[WIP] Multiplayer Support (using the Online mod)",
             "[WIP] [Multi] Live Spectator Support",
             "[Multi] Head to Head Support",
             "[WIP] [Multi] CO-OP Support",
             "[WIP] [Multi] TAG Support",
-            "[WIP] [Multi] PVP Support",
+            "[WIP] [Multi] PVP Support\n",
+
             "OSX Support (Really this was the modloader's fault...)",
             "iOS Support",
-            "Touch screen controls (they are a toggle in settings for now)",
             //"Linux Support",
-            //"Added classic enemy visuals",
+            //"Android Support",
+            "Touch screen controls (they are a toggle in settings for now)\n",
+            
             "Upgraded Changelog again (and will start backfilling old versions back to pre 0.1.0)",
             "New Experimental code trying to fix performance issues",
             "Removed Storymode",
@@ -38,7 +42,7 @@
 
         protected override string[] TweaksAndChanges => new[]
         {
-            "Made shooting more responsive",
+            "Made shooting more responsive (now it will always fire when you click, and if you hold it will auto fire on half beats)",
             "Brought back some old wiki sections on mapping and multiplayer (wonder what those could be for...)",
             "Get Time.Current less (Bullets + Enemies hurt fps less now)",
         };
